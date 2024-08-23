0c7a881e76627cd71aab50079398d742e657ff28
Nicole Inaê de Oliveira <127264038+nicoleinae@users.noreply.github.com>
2024-08-23 19:36:10 -0300

    Criando projeto com metodo post e get

---
+++ planner/academico/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Professor(models.Model):
+    nome = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nome
+
+class Curso(models.Model):
+    nome = models.CharField(max_length=200)
+    sigla = models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.nome
